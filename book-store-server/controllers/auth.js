520a07eabd53db54f1160baf59979cd83f9d718e
misa198 <csnsjh@gmail.com>
2020-02-17 21:31:12 +0700

    User model

--- book-store-server/controllers/auth.js
+++ book-store-server/controllers/auth.js
@@ -1,3 +1,12 @@
+const { registerValidation, loginValidate } = require('../validations/auth')
+
 const register = async (req, res) => {
+    // Validate data send to server
+    const { errors } = registerValidation(req.body);
+
+    if (errors)
+        return res.send({ message: errors });
+
+    // Checking user already exists
     
 }
---
+++ book-store-server/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 3
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 6
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 8
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
