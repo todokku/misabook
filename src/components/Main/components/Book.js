b03da62b15ccbd8d9faec3f4c04d91d8dcbec537
misa198 <csnsjh@gmail.com>
2020-02-19 01:18:01 +0700

    home

--- src/components/Main/components/Book.js
+++ src/components/Main/components/Book.js
@@ -6,20 +6,27 @@ import './Book.css';
 const Book = (props) => {
     return (
         <div className='Book'>
-            <Link to={`/book/${props.book._id}`}
+            <Link className='book-cover-link' to={`/book/${props.book._id}`}
                 
             >
                 <div className='book-cover'
                     style={{backgroundImage: `url(${props.book.cover})`}}
                 >
-                    {/* <img src={props.book.cover} alt={props.book.name}/> */}
                 </div>
             </Link>
             <div className='book-infor'>
                 <Link to={`/book/${props.book._id}`} >
                     <h5 className='book-name'>{props.book.name}</h5>
                 </Link>
-                <h5 className='book-price'>{`${props.book.price} $`}</h5>
+                <div className='cart-wrapper'>
+                    <h5 className='book-price'>{`${props.book.price} $`}</h5>
+                    <button>
+                        <div>
+                            <i className="material-icons">add_shopping_cart</i>
+                        </div>
+                        <div>Add to cart</div>
+                    </button>
+                </div>
             </div>
         </div>
     );
