436049700929e1d4be3a78f7fe25f9bf3d508427
misa198 <csnsjh@gmail.com>
2020-02-20 01:12:34 +0700

    about-page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom';
 import Home from '../../pages/Home/Home';
 import Books from '../../pages/Books/Books';
 import BookViewer from '../../pages/BookViewer/BookViewer';
+import About from '../../pages/About/About';
 
 import './Main.css';
 
@@ -13,6 +14,7 @@ const Main = () => {
             <Route exact path='/' component={Home} />
             <Route exact path='/books' component={Books} />
             <Route path='/books/:id' component={BookViewer} />
+            <Route exact path='/about' component={About} />
         </div>
     );
 }
