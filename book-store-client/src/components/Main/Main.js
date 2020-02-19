fe100e505fb622baeb929147a6537a36b9eb2229
misa198 <csnsjh@gmail.com>
2020-02-20 02:14:23 +0700

    Contact update

--- book-store-client/src/components/Main/Main.js
+++ book-store-client/src/components/Main/Main.js
@@ -5,6 +5,7 @@ import Home from '../../pages/Home/Home';
 import Books from '../../pages/Books/Books';
 import BookViewer from '../../pages/BookViewer/BookViewer';
 import About from '../../pages/About/About';
+import Contact from '../../pages/Contact/Contact';
 
 import './Main.css';
 
@@ -15,6 +16,7 @@ const Main = () => {
             <Route exact path='/books' component={Books} />
             <Route path='/books/:id' component={BookViewer} />
             <Route exact path='/about' component={About} />
+            <Route exact path='/contact' component={Contact} />
         </div>
     );
 }
---
+++ book-store-client/src/pages/Contact/Contact.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+import './Contact.css';
+
+const Contact = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const nameRegex = /^.{3,}$/; // Minium 3 characters
+    // eslint-disable-next-line
+    const emailRegex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    // eslint-disable-next-line
+
+    const updateName = (e) => {
+        setName(e.target.value);
+    }
+
+    const updateEmail = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const updateMessage = (e) => {
+        setMessage(e.target.value);
+    }
+
+    return (
+        <div className='Contact'>
+            <h3 className='title'>Contact</h3>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id='name' placeholder='name...' required
+                        onChange={updateName}
+                    />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="text" id='email' placeholder='email...' required
+                        onChange={updateEmail}
+                    />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="message">Message</label>
+                    <textarea name="message" id="message" cols="30" rows="10" required
+                        onChange={updateMessage}
+                    ></textarea>
+                </div>
+                <button>Send</button>
+            </form>
+        </div>
+    );
+}
+
+export default Contact;
