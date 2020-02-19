fdd317722cc210771d4c4c12ca26e0238fd7fc09
misa198 <csnsjh@gmail.com>
2020-02-20 05:38:14 +0700

    get cart books

--- book-store-client/src/pages/Cart/Cart.js
+++ book-store-client/src/pages/Cart/Cart.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import axios from 'axios';
+
+import { AuthContext } from '../../context/AuthContext';
+import { CartContext } from '../../context/CartContext';
 
 import './Cart.css';
 
 const Cart = () => {
+    const [token, setToken] = useContext(AuthContext);
+    const { items } = useContext(CartContext);
+    
+    useEffect(() => {
+        if (items[0].length !== 0) {
+            axios.post('/books/ids', {
+                items: items[0] 
+            }, {
+                headers: {
+                    token: token
+                }
+            })
+            .then(res => {
+                console.log(res);
+            })
+        }
+    }, [items])
+
     return (
         <div className='Cart'>
-            
+            <h3>Cart</h3>
+            <div className='container'>
+
+            </div>
         </div>
     );
 }
