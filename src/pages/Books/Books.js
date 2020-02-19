fd64de42f62da2a39dec82ce55ff3fc1b4097f78
misa198 <csnsjh@gmail.com>
2020-02-19 22:11:06 +0700

    add loadmore

--- src/pages/Books/Books.js
+++ src/pages/Books/Books.js
@@ -14,29 +14,47 @@ const Books = () => {
     const [page, setPage] = useState(1);
     const [field, setField] = useState('name');
     const [sort, setSort] = useState(1);
+    const [loadMoreShowed, setLoadMoreShowed] = useState(false);
 
     const [books, setBooks] = useState([]);
-
+    
+    
     useEffect(() => {
         axios.get(`/books/all?page=${page}&field=${field}&sort=${sort}`, { headers: {
             token: token
         } })
         .then((res) => {
-            setBooks(res.data);
+            setLoadMoreShowed(true);
+            if (res.data.length === 0)
+                setLoadMoreShowed(false);
+            setBooks(books.concat(res.data));
         });
-    }, []);
+        // eslint-disable-next-line
+    }, [field, sort, page, token]);
+
+    const updateField = (e) => {
+        setField(e.target.value);
+    }
+    
+    const updateSort = (e) => {
+        setSort(e.target.value);
+    }
+
+    const loadMore = () => {
+        setPage(page + 1);
+    }
 
     return (
         <div className='Books'>
             <h3 className='title'>Books</h3>
             <div className='books-selector'>
                 <span className='title'>Sort by:</span>
-                <select name="field" id="field">
+                <select name="field" id="field" onChange={updateField}>
                     <option value="name">Name</option>
                     <option value="price">Price</option>
                     <option value="seller">Turnover</option>
                 </select>
-                <select name="sort" id="sort">
+                <select name="sort" id="sort" onChange={updateSort}>
                     <option value="1">Ascending</option>
                     <option value="-1">Decrease</option>
                 </select>
@@ -46,6 +64,11 @@ const Books = () => {
                     <Book book={book} key={book._id} />
                 ))}
             </div>
+            {loadMoreShowed &&
+                <div className='load-more'>
+                    <button onClick={loadMore}>Load more</button>
+                </div>
+            }
         </div>
     );
 }
