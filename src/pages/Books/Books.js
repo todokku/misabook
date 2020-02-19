0efbd448f2a2f508ae8a203d9284aa7fa34e91b1
misa198 <csnsjh@gmail.com>
2020-02-19 21:37:56 +0700

    selector

--- src/pages/Books/Books.js
+++ src/pages/Books/Books.js
@@ -11,20 +11,36 @@ const Books = () => {
     // eslint-disable-next-line
     const [token, setToken] = useContext(AuthContext);
 
+    const [page, setPage] = useState(1);
+    const [field, setField] = useState('name');
+    const [sort, setSort] = useState(1);
+
     const [books, setBooks] = useState([]);
 
     useEffect(() => {
-        axios.get('/books', { headers: {
+        axios.get(`/books/all?page=${page}&field=${field}&sort=${sort}`, { headers: {
             token: token
         } })
         .then((res) => {
             setBooks(res.data);
         });
-    });
+    }, []);
 
     return (
         <div className='Books'>
             <h3 className='title'>Books</h3>
+            <div className='books-selector'>
+                <span className='title'>Sort by:</span>
+                <select name="field" id="field">
+                    <option value="name">Name</option>
+                    <option value="price">Price</option>
+                    <option value="seller">Turnover</option>
+                </select>
+                <select name="sort" id="sort">
+                    <option value="1">Ascending</option>
+                    <option value="-1">Decrease</option>
+                </select>
+            </div>
             <div className='content'>
                 {books.map(book => (
                     <Book book={book} key={book._id} />
