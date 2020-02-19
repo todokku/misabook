7ed8652a8666cb4a45d69ff6490b2a480236d715
misa198 <csnsjh@gmail.com>
2020-02-20 03:24:33 +0700

    Contact post update

--- book-store-client/src/pages/Contact/Contact.js
+++ book-store-client/src/pages/Contact/Contact.js
@@ -1,11 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import classnames from 'classnames';
+import axios from 'axios';
+
+import { AuthContext } from '../../context/AuthContext';
+
+import successImg from '../../assets/success.png';
 
 import './Contact.css';
 
 const Contact = () => {
+    // eslint-disable-next-line
+    const [token, setToken] = useContext(AuthContext);
+
+    const [success, setSuccess] = useState(false);
+
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
+
+    const [nameError, setNameError] = useState(false);
+    const [emailError, setEmailError] = useState(false);
 
     const nameRegex = /^.{3,}$/; // Minium 3 characters
     // eslint-disable-next-line
@@ -24,22 +38,72 @@ const Contact = () => {
         setMessage(e.target.value);
     }
 
+    const submit = (e) => {
+        e.preventDefault();
+
+        setNameError(false);
+        setEmailError(false);
+
+        let errors = 0;
+
+        if (!nameRegex.test(name)) {
+            errors++;
+            setNameError(true);
+        }
+
+        if (!emailRegex.test(email)) {
+            errors++;
+            setEmailError(true);
+        }
+        
+        if (errors === 0) {
+            axios.post('/message', {
+                name: name,
+                email: email,
+                message: message
+            }, { headers: {
+                token: token
+            } })
+            .then((res) => {
+                if (!res.data.message) {
+                    setSuccess(true);
+                }
+            });
+        }
+    }
+
+    useEffect(() => {
+        if (success) {
+            setTimeout(() => {
+                setSuccess(false);
+            }, 2500);
+        }
+    }, [success])
+
     return (
         <div className='Contact'>
-            <div className='content'>
+            <div className={classnames('content', {
+                'content-hiden': success
+            })}>
                 <h3 className='title'>Contact</h3>
-                <form>
+                <form onSubmit={submit}>
                     <div className="form-control">
                         <label htmlFor="name">Name</label>
                         <input type="text" id='name' placeholder='name...' required
                             onChange={updateName}
                         />
+                        <div className={classnames('errors', {
+                            'errors-showed': nameError
+                        })}>Name is invalid</div>
                     </div>
                     <div className="form-control">
                         <label htmlFor="email">Email</label>
                         <input type="text" id='email' placeholder='email...' required
                             onChange={updateEmail}
                         />
+                        <div className={classnames('errors', {
+                            'errors-showed': emailError
+                        })}>Email is invalid</div>
                     </div>
                     <div className="form-control">
                         <label htmlFor="message">Message</label>
@@ -49,6 +113,12 @@ const Contact = () => {
                     </div>
                     <button>Send</button>
                 </form>
+            </div>
+            <div className={classnames('success', {
+                'success-showed': success
+            })}>
+                <img src={successImg} alt="success"/>
+                <div className='success-title'>Success</div>
             </div>
         </div>
     );
