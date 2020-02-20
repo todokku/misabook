66d42c8d6594dc475331f046d4af65bde885b512
misa198 <csnsjh@gmail.com>
2020-02-20 17:29:30 +0700

    order save and update seller

--- book-store-server/controllers/order.js
+++ book-store-server/controllers/order.js
@@ -1,4 +1,6 @@
+const mongoose = require('mongoose');
 const Order = require('../models/Order');
+const Book = require('../models/Book');
 const orderValidation = require('../validations/order');
 
 const getAllOrder = async (req, res) => {
@@ -12,19 +14,42 @@ const getAllOrder = async (req, res) => {
 }
 
 const insertOrder = async (req, res) => {
-    try {
-        const newOrder = new Order({
-            name: req.body.name,
-            'phone-number': req.body.phoneNumber,
-            address: req.body.address,
-            items: req.body.items
-        });
-        
-        const savedOrder = await newOrder.save();
-        res.send(savedOrder);
+    const { errors } = orderValidation(req.body);
+
+    if (!errors) {
+        try {
+            // Create new order
+            const newOrder = new Order({
+                name: req.body.name,
+                'phone-number': req.body.phoneNumber,
+                address: req.body.address,
+                items: req.body.items
+            });
+
+            const items = req.body.items;
+    
+            // Update seller in database
+            for (let i = 0; i < items.length; i++) {
+                const preBook = await Book.findOne({ _id: items[i].id })
+                console.log(preBook)
+                const newSeller = preBook.seller + items[i].amount;
+                await Book.updateOne(
+                    { _id: mongoose.Types.ObjectId(items[i].id) },
+                    { $set: {
+                        seller: newSeller
+                    } }
+                );
+            }
+
+            const savedOrder = await newOrder.save();
+            res.send(savedOrder);
+        }
+        catch(error) {
+            res.send({ message: error });
+        }
     }
-    catch(error) {
-        res.send({ message: error });
+    else {
+        res.send({ message: errors });
     }
 }
 
