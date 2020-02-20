be0240ba91667a3e78ed40588bd6e002200b8234
misa198 <csnsjh@gmail.com>
2020-02-20 11:24:55 +0700

    fix auth route

--- book-store-client/src/pages/Auth/Auth.js
+++ book-store-client/src/pages/Auth/Auth.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 
 import Login from './components/Login';
 import Register from './components/Register';
@@ -8,27 +10,58 @@ import './Auth.css';
 const Auth = () => {
     const background = 'https://res.cloudinary.com/dumfvnj9f/image/upload/q_66/v1582020322/bookstore/background_jc5hqx.png';
 
+    const [isLogined, setIsLogined] = useState(undefined);
     const [register, setRegister] = useState(false);
 
     const swithAuth = () => {
         setRegister(!register);
     }
 
-    return (
-        <div className='Auth'>
-            <div id='cover' style={{ backgroundImage: `url(${background})`}}></div>
-            <div id='content'>
-                <Login 
-                    swithAuth={swithAuth}
-                    register={register}
-                />
-                <Register 
-                    swithAuth={swithAuth}
-                    register={register}
-                />
+    useEffect(() => {
+        let token = localStorage.getItem('token');
+        if (token) {
+            axios.post('/auth/user', {
+                token: token
+            })
+            .then(res => {
+                console.log(res)
+                if (res.data.message === 'Successfully') {
+                    setIsLogined(true);
+                }
+                else
+                    setIsLogined(false);
+            })
+        }
+    }, []);
+
+    if (isLogined === undefined) {
+        return (
+            <div></div>
+        )
+    }
+
+    if (isLogined) {
+        return (
+            <Redirect to='/'/>
+        )
+    }
+    else if (!isLogined) {
+        return (
+            <div className='Auth'>
+                <div id='cover' style={{ backgroundImage: `url(${background})`}}></div>
+                <div id='content'>
+                    <Login 
+                        swithAuth={swithAuth}
+                        register={register}
+                    />
+                    <Register 
+                        swithAuth={swithAuth}
+                        register={register}
+                    />
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Auth;
