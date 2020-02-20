9ef382c3910cf9a48bb8da0834d0d6d525a64ebc
misa198 <csnsjh@gmail.com>
2020-02-20 11:28:22 +0700

    order model

---
+++ book-store-server/models/Order.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const orderSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    'phone-number': {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    order: {
+        type: Object,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
