271d87b954fa0beba32dd9f102ce73757521695c
misa198 <csnsjh@gmail.com>
2020-02-20 17:52:23 +0700

    fix

--- book-store-server/index.js
+++ book-store-server/index.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Middlewares
+app.disable('x-powered-by');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
